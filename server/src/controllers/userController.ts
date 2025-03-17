d23078beba68b05cbc89308462df0a0c9796fd5e
Golf Management System <golf_mgmt@example.com>
2025-03-17 23:10:05 +0800

    將 bcrypt 替換為 bcryptjs 以解決部署問題

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { pool } from '../config/db';
 import { generateToken, generateRefreshToken } from '../utils/jwtUtils';
 
